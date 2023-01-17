ffbe36c2c8c4c5a34ee6285f8651ca6860496cb1
Naresh Gohil <nareshngohil99@gmail.com>
2023-01-17 12:59:09 +0530

    completed

---
+++ ReactAssign1/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React from 'react'
+import Logo from './Components/Logo';
+import Search from './Components/Search';
+import Language from './Components/Language';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Logo/>
+     <Search/>
+     <Language/>
+    </div>
+  )
+}
+
+
+
+
+
+
+export default App;
